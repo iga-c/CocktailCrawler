b57419cd339dfab606f5deb0a03ff567328f7395
iga-c <k.igari1013@gmail.com>
2017-07-06 22:07:20 +0900

    :green_heart: rubocop all green.

--- cocktail.rb
+++ cocktail.rb
@@ -10,20 +10,20 @@ class Cocktail
     doc = Nokogiri::HTML(File.read(html))
 
     @name = doc.xpath('//div[@class="cnt"]/strong').text
-    @make = doc.xpath("/html/body/table/tr/td[2]/div[8]/table/tr/td[2]").text
+    @make = doc.xpath('/html/body/table/tr/td[2]/div[8]/table/tr/td[2]').text
     @materials = doc.xpath('//table/tr/td[@width="50"]/div[@align="center"]/../../../tr')[1..-1]
-                    .map{|item| item.xpath("td") }
-                    .map{|td| Material.new(td[0].text.strip, td[1..-1].text) }
+                    .map { |item| item.xpath('td') }
+                    .map { |td| Material.new(td[0].text.strip, td[1..-1].text) }
   end
 
   def create?(my_materials)
-    @materials.map(&:name).all?{|item| my_materials.include?(item) }
+    @materials.map(&:name).all? { |item| my_materials.include?(item) }
   end
 
   def recipe
     print("カクテル名：#{@name}\n")
     print("材料：\n")
-    @materials.each{|material| print("#{material.name}：#{material.volume}\n") }
+    @materials.each { |material| print("#{material.name}：#{material.volume}\n") }
     print("作り方：\n")
     print("#{@make}\n")
   end
--- parser.rb
+++ parser.rb
@@ -5,8 +5,8 @@ HTML_DIR = File.expand_path('../html', __FILE__)
 if $PROGRAM_NAME == __FILE__
   my_materials = File.read(File.expand_path('../my_materials.txt', __FILE__)).strip.split("\n").map(&:strip)
   cocktails = Dir.glob(File.join(HTML_DIR, '*recipe*'))
-                 .map {|html| Cocktail.new(html) }
-                 .select {|cocktail| cocktail.create?(my_materials) }
+                 .map { |html| Cocktail.new(html) }
+                 .select { |cocktail| cocktail.create?(my_materials) }
 
-  cocktails.each{|cocktail| print("#{cocktail.recipe}\n") }
+  cocktails.each { |cocktail| print("#{cocktail.recipe}\n") }
 end
